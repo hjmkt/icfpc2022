63eab9e935faa472cd0c6a0338e9077b5cea9ac3
Hajime Kato <hjmkt3126@gmail.com>
2022-09-03 03:14:48 +0900

    Update sample test script

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ canvas = Canvas(400, 400)
 
 moves = [
     Move("lcut", {"block_id": "0", "orientation": "horizontal", "offset": 342}),
-    Move("lcut", {"block_id": "0.0", "orientation": "horizontal", "offset": 142}),
+    Move("lcut", {"block_id": "0.0", "orientation": "horizontal", "offset": 124}),
     Move("lcut", {"block_id": "0.0.0", "orientation": "horizontal", "offset": 65}),
     Move("color", {"block_id": "0.0.0.1", "color": [226, 227, 225, 255]}),
     Move("color", {"block_id": "0.0.1", "color": [91, 91, 85, 255]}),
@@ -15,11 +15,12 @@ moves = [
 target_image = cv2.imread("./6.png", cv2.IMREAD_UNCHANGED)
 target_image = target_image[::-1, :, :]
 target_image = cv2.cvtColor(target_image, cv2.COLOR_BGRA2RGBA)
+# print(target_image.shape)
 
-similarity = canvas.compute_similarity(target_image)
-print(f"similarity = {similarity}")
-cv2.imshow("pre", canvas.pixels.astype(np.uint8))
-cv2.waitKey(0)
+# similarity = canvas.compute_similarity(target_image)
+# print(f"similarity = {similarity}")
+# cv2.imshow("pre", canvas.pixels.astype(np.uint8))
+# cv2.waitKey(0)
 
 for move in moves:
     canvas.exec_move(move)
@@ -27,7 +28,10 @@ for move in moves:
 
 cost = canvas.get_current_cost()
 similarity = canvas.compute_similarity(target_image)
+score = canvas.compute_score(target_image)
 print(f"cost = {cost}")
 print(f"similarity = {similarity}")
-cv2.imshow("post", cv2.cvtColor(canvas.pixels.astype(np.uint8), cv2.COLOR_BGRA2RGBA))
-cv2.waitKey(0)
+print(f"score = {score}")
+# cv2.imshow("post", cv2.cvtColor(canvas.pixels.astype(np.uint8)[::-1, :, :], cv2.COLOR_BGRA2RGBA))
+# cv2.imwrite("post.png", cv2.cvtColor(canvas.pixels.astype(np.uint8)[::-1, :, :], cv2.COLOR_BGRA2RGBA))
+# cv2.waitKey(0)
