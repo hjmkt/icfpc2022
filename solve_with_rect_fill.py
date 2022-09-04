bef72e93deb387e2a812b644042762052b49d74a
Hajime Kato <hjmkt3126@gmail.com>
2022-09-04 16:34:56 +0900

    Update solve_with_rect_fill to support problem>=26

--- solve_with_rect_fill.py
+++ solve_with_rect_fill.py
@@ -4,6 +4,8 @@ from icfpc2022_api import *
 import cv2
 import sys
 import time
+import copy
+import json
 from rect_fill import *
 import argparse
 
@@ -14,13 +16,69 @@ parser.add_argument("-m", "--merge", type=int)
 parser.add_argument("-t", "--token", type=str)
 args = parser.parse_args()
 
-canvas = Canvas(400, 400)
+width = 400
+height = 400
+
 
 target_image = cv2.imread(f"./{args.problem}.png", cv2.IMREAD_UNCHANGED)
 target_image = target_image[::-1, :, :]
 target_image = cv2.cvtColor(target_image, cv2.COLOR_BGRA2RGBA)
 
-moves = []
+canvas = Canvas(width, height)
+canvas_for_cost = Canvas(width, height)
+global_id = 0
+
+initial_moves = []
+if args.problem>=26:
+    with open(f"{args.problem}.initial.json") as f:
+        initial_config = json.load(f)
+        width = initial_config["width"]
+        height = initial_config["height"]
+        num_blocks = len(initial_config["blocks"])
+        if num_blocks==100:
+            block_width = 40
+            block_height = 40
+            rows = 10
+            cols = 10
+        else:
+            block_width = 16
+            block_height = 16
+            rows = 25
+            cols = 25
+        for row in range(rows):
+            for col in range(cols-1):
+                id0 = f"{row*cols}" if col==0 else f"{rows*cols+row*(cols-1)+col-1}"
+                id1 = f"{row*cols+col+1}"
+                initial_moves.append(Move("merge", {"block_id0": id0, "block_id1": id1}))
+        for row in range(rows-1):
+            id0 = f"{rows*cols-1+cols-1}" if row==0 else f"{rows*cols-1+rows*(cols-1)+row}"
+            id1 = f"{rows*cols-1+(row+2)*(cols-1)}"
+            initial_moves.append(Move("merge", {"block_id0": id0, "block_id1": id1}))
+        canvas = Canvas(width, height)
+        canvas_for_cost = Canvas(width, height)
+        global_id = rows*cols - 1
+        canvas.global_id = global_id
+        canvas_for_cost.global_id = global_id
+        for row in range(rows):
+            for col in range(cols):
+                block = initial_config["blocks"][row*cols+col]
+                x0, y0 = block["bottomLeft"]
+                x1, y1 = block["topRight"]
+                canvas.pixels[y0:y1, x0:x1] = block["color"]
+                canvas_for_cost.pixels[y0:y1, x0:x1] = block["color"]
+                canvas.blocks[block["blockId"]] = SimpleBlock(x0, y0, x1-x0, y1-y0, block["color"], block["blockId"])
+                canvas.all_blocks[block["blockId"]] = SimpleBlock(x0, y0, x1-x0, y1-y0, block["color"], block["blockId"])
+                canvas_for_cost.blocks[block["blockId"]] = SimpleBlock(x0, y0, x1-x0, y1-y0, block["color"], block["blockId"])
+                canvas_for_cost.all_blocks[block["blockId"]] = SimpleBlock(x0, y0, x1-x0, y1-y0, block["color"], block["blockId"])
+                for y in range(y0, y1):
+                    for x in range(x0, x1):
+                        canvas.coord_to_block_id[y][x] = block["blockId"]
+                        canvas_for_cost.coord_to_block_id[y][x] = block["blockId"]
+        for move in initial_moves:
+            # print(move.move_type, move.options)
+            canvas.exec_move(move)
+
+moves = copy.copy(initial_moves)
 while True:
     (cand_rect, cand_score_diff) = find_cand_rect(canvas, target_image, args.seed, args.merge)
     print(cand_rect, cand_score_diff, file=sys.stderr)
@@ -36,7 +94,6 @@ while True:
 while moves[-1].move_type=="merge":
     moves = moves[:-1]
 
-canvas_for_cost = Canvas(400, 400)
 for move in moves:
     canvas_for_cost.exec_move(move)
 cost = canvas_for_cost.get_current_cost()
