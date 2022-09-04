b62bb15bece28b1601f5d1921ede5cc637c99da2
lambdataro <lambda.taro@gmail.com>
2022-09-05 00:03:04 +0900

    問題40まで追加

--- interactive-v2/tools/downloadProblems.js
+++ interactive-v2/tools/downloadProblems.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
-const maxProblems = 35;
+const maxProblems = 40;
 
 async function main() {
     for (let i = 1; i <= maxProblems; i++) {
@@ -25,6 +25,20 @@ async function main() {
                     console.log(`${i}.initial.json`);
                     fs.writeFileSync(
                         path.join("static", `${i}.initial.json`),
+                        JSON.stringify(response.data),
+                        { encoding: "utf-8" }
+                    );
+                });
+        }
+
+        if (i >= 36) {
+            await axios
+                .get(`https://cdn.robovinci.xyz/sourcepngs/${i}.source.json`)
+                .then((response) => {
+                    // console.log(response);
+                    console.log(`${i}.source.json`);
+                    fs.writeFileSync(
+                        path.join("static", `${i}.source.json`),
                         JSON.stringify(response.data),
                         { encoding: "utf-8" }
                     );
