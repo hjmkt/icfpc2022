Fix cost calculation in rect_fill

solve_with_rect_fill.py
@@ -3,16 +3,23 @@ from util import *
 import cv2
 import sys
 from rect_fill import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--problem", type=int)
+parser.add_argument("-s", "--seed", type=int)
+parser.add_argument("-m", "--merge", type=int)
+args = parser.parse_args()
 
 canvas = Canvas(400, 400)
 
-target_image = cv2.imread("./11.png", cv2.IMREAD_UNCHANGED)
+target_image = cv2.imread(f"./{args.problem}.png", cv2.IMREAD_UNCHANGED)
 target_image = target_image[::-1, :, :]
 target_image = cv2.cvtColor(target_image, cv2.COLOR_BGRA2RGBA)
 
 moves = []
 while True:
-    (cand_rect, cand_score_diff) = find_cand_rect(canvas, target_image)
+    (cand_rect, cand_score_diff) = find_cand_rect(canvas, target_image, args.seed, args.merge)
     print(cand_rect, cand_score_diff, file=sys.stderr)
     rect_moves = rect_to_moves(canvas, cand_rect)
     if cand_score_diff<0 and len(rect_moves)>0:
@@ -39,4 +46,6 @@ print(f"score = {score}", file=sys.stderr)
 cv2.imwrite("post.png", cv2.cvtColor(canvas.pixels.astype(np.uint8)[::-1, :, :], cv2.COLOR_BGRA2RGBA))
 # cv2.waitKey(0)
 isl = moves_to_isl(moves)
-print(isl)
+with open(f"./isl_p{args.problem}_{round(score)}.txt", "w") as f:
+    print(isl, file=f)
+# print(isl)